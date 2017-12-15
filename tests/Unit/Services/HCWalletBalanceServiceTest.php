19a30a686b090feeacaee7d91f21b3c3195d0f87
Mantas Paulauskas <mantopastas@gmail.com>
2017-12-15 17:10:37 +0200

    added wallet balance service tests

--- tests/Unit/Services/HCWalletBalanceServiceTest.php
+++ tests/Unit/Services/HCWalletBalanceServiceTest.php
@@ -29,16 +29,109 @@ declare(strict_types = 1);
 
 namespace Tests\Unit\Services;
 
+use Faker\Generator as Faker;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use InteractiveSolutions\HoneycombAcl\Models\HCUsers;
+use InteractiveSolutions\HoneycombWallet\Exceptions\WalletNotEnoughBalanceException;
+use InteractiveSolutions\HoneycombWallet\Models\HCWallet;
+use InteractiveSolutions\HoneycombWallet\Models\Wallet\HCWalletHistory;
 use InteractiveSolutions\HoneycombWallet\Services\HCWalletBalanceService;
 use Tests\TestCase;
 
 class HCWalletBalanceServiceTest extends TestCase
 {
+    use DatabaseMigrations;
+
+    /**
+     *
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->factory->define(HCUsers::class, function(Faker $faker) {
+            static $password;
+
+            return [
+                'email' => $faker->email,
+                'password' => $password ?: $password = bcrypt('secret'),
+                'remember_token' => str_random(10),
+                'last_visited' => $faker->dateTime,
+            ];
+        });
+
+        $this->factory->define(HCWallet::class, function(Faker $faker) {
+            return [
+                'balance' => rand(1, 1000),
+                'balance_debit' => 0,
+                'ownable_id' => function() {
+                    return factory(HCUsers::class)->create()->id;
+                },
+                'ownable_type' => HCUsers::class,
+            ];
+        });
+    }
+
+    /** @test */
+    public function increase_method_must_return_success_on_creating_wallet(): void
+    {
+        $wallet = factory(HCWallet::class)->create();
+
+        $increaseAmount = 20;
+
+        $response = $this->getServiceInstance()->increase($increaseAmount, $wallet->ownable_id, $wallet->ownable_type);
+
+        $this->assertTrue($response['success']);
+
+        $this->assertTrue($response['wallet'] instanceof HCWallet);
+
+        $this->assertTrue($response['history'] instanceof HCWalletHistory);
+
+        $this->assertEquals($wallet->balance + $increaseAmount, $response['wallet']->balance);
+
+        $this->assertEquals(0, $response['wallet']->balance_debit);
+
+        $this->assertEquals($wallet->ownable_id, $response['wallet']->ownable_id);
+
+        $this->assertEquals($wallet->ownable_type, $response['wallet']->ownable_type);
+    }
+
+    /** @test */
+    public function it_must_throw_exception_when_wallet_balance_is_lower_than_given_amount(): void
+    {
+        $this->expectException(WalletNotEnoughBalanceException::class);
+
+        $this->expectExceptionMessage(trans('HCWallet::wallet.errors.not_enough_amount', [
+            'balance' => 10,
+            'amount' => 20,
+        ]));
+
+        $wallet = factory(HCWallet::class)->create([
+            'balance' => 10,
+        ]);
+
+        $this->getServiceInstance()->decrease(20, $wallet->ownable_id, $wallet->ownable_type);
+    }
+
+    /** @test */
+    public function it_must_return_success_when_decreasing_form_wallet_with_valid_amount()
+    {
+        $wallet = factory(HCWallet::class)->create(['balance' => 10]);
+
+        $response = $this->getServiceInstance()->decrease(5, $wallet->ownable_id, $wallet->ownable_type);
+
+        $this->assertTrue($response['success']);
+
+        $this->assertEquals(5, $response['wallet']->balance);
+
+        $this->assertEquals(0, $response['wallet']->balance_debit);
+    }
+
     /**
      * @return HCWalletBalanceService
      */
     private function getServiceInstance(): HCWalletBalanceService
     {
-        return $this->app->make(HCServiceController::class);
+        return $this->app->make(HCWalletBalanceService::class);
     }
 }
